058dce3423cb3f9886ad70ab145a17f2d269fe00
Elberte Plínio <elbertedoolan@gmail.com>
2022-10-05 17:21:33 -0300

    Ensure NumberTriviaModel return a valid model with JSON integer number

--- test/features/number_trivia/data/models/number_trivia_model_test.dart
+++ test/features/number_trivia/data/models/number_trivia_model_test.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter_clean_architecture_tdd_course/features/number_trivia/data/models/number_trivia_model.dart';
 import 'package:flutter_clean_architecture_tdd_course/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:flutter_test/flutter_test.dart';
+
+import '../../../../fixtures/fixture_reader.dart';
 
 void main() {
   final tNumberTriviaModel = NumberTriviaModel(number: 1, text: 'Test Text');
@@ -12,4 +16,20 @@ void main() {
       expect(tNumberTriviaModel, isA<NumberTrivia>());
     },
   );
+
+  group('fromJson', () {
+    test(
+      'Should return a valid model when the JSON number is an integer',
+      () async {
+        //arrange
+        final Map<String, dynamic> jsonMap =
+            json.decode(fixture('trivia.json'));
+        //act
+        final result = NumberTriviaModel.fromJson(jsonMap);
+
+        //assert
+        expect(result, tNumberTriviaModel);
+      },
+    );
+  });
 }
