0961de52db5d24038ed74db8cb2f156c1024935e
Elberte Plínio <elbertedoolan@gmail.com>
2022-10-05 16:50:05 -0300

    Ensure NumberTriviaModel is a subclass of NumberTrivia

---
+++ lib/features/number_trivia/data/models/number_trivia_model.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_clean_architecture_tdd_course/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:meta/meta.dart';
+
+class NumberTriviaModel extends NumberTrivia {
+  NumberTriviaModel({
+    @required String text,
+    @required int number,
+  }) : super(text: text, number: number);
+}
