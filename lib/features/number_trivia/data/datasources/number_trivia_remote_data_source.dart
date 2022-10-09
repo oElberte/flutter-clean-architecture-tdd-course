3c741237357a5b42522969e6aedacf4d0b419e7a
Elberte Plínio <elbertedoolan@gmail.com>
2022-10-09 15:06:16 -0300

    Refactor NumberTriviaRemoteDataSource

--- lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
+++ lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
@@ -24,23 +24,16 @@ class NumberTriviaRemoteDataSourceImpl implements NumberTriviaRemoteDataSource {
   NumberTriviaRemoteDataSourceImpl({@required this.client});
 
   @override
-  Future<NumberTriviaModel> getConcreteNumberTrivia(int number) async {
-    final response = await client.get(
-      'http://numbersapi.com/$number',
-      headers: {'Content-Type': 'application/json'},
-    );
-
-    if (response.statusCode == 200) {
-      return NumberTriviaModel.fromJson(json.decode(response.body));
-    } else {
-      throw ServerException();
-    }
-  }
+  Future<NumberTriviaModel> getConcreteNumberTrivia(int number) =>
+      _getTriviaFromUrl('http://numbersapi.com/$number');
 
   @override
-  Future<NumberTriviaModel> getRandomNumberTrivia() async {
+  Future<NumberTriviaModel> getRandomNumberTrivia() =>
+      _getTriviaFromUrl('http://numbersapi.com/random');
+
+  Future<NumberTriviaModel> _getTriviaFromUrl(String url) async {
     final response = await client.get(
-      'http://numbersapi.com/random',
+      url,
       headers: {'Content-Type': 'application/json'},
     );
 
