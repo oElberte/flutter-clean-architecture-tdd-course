875106802061aa5bffec6cccdeafcd85cf0f76ff
Elberte Plínio <elbertedoolan@gmail.com>
2022-10-09 16:48:00 -0300

    Ensure stringToUnsignedInt return an integer from a unsigned integer

---
+++ test/core/util/input_converter_test.dart
@@ -0,0 +1,25 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_clean_architecture_tdd_course/core/util/input_converter.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  InputConverter inputConverter;
+
+  setUp(() {
+    inputConverter = InputConverter();
+  });
+
+  group('stringToUnsignedInt', () {
+    test(
+      'Should return an integer when the string represents an unsigned integer',
+      () async {
+        //arrange
+        final str = '123';
+        //act
+        final result = inputConverter.stringToUnsignedInteger(str);
+        //assert
+        expect(result, Right(123));
+      },
+    );
+  });
+}
