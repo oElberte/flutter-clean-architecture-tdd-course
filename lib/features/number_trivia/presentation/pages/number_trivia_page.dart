4d4c6c0ee1765e5ab425524e86e35c2175814ec2
Elberte Plínio <elbertedoolan@gmail.com>
2022-10-10 17:46:05 -0300

    Button to search for a specific number trivia working

--- lib/features/number_trivia/presentation/pages/number_trivia_page.dart
+++ lib/features/number_trivia/presentation/pages/number_trivia_page.dart
@@ -41,28 +41,68 @@ class NumberTriviaPage extends StatelessWidget {
               ),
               SizedBox(height: 20),
               // Bottom half
-              Column(
-                children: <Widget>[
-                  // TextField
-                  Placeholder(fallbackHeight: 40),
-                  SizedBox(height: 10),
-                  Row(
-                    children: <Widget>[
-                      Expanded(
-                        child: Placeholder(fallbackHeight: 30),
-                      ),
-                      SizedBox(width: 10),
-                      Expanded(
-                        child: Placeholder(fallbackHeight: 30),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+              TriviaControls(),
             ],
           ),
         ),
       ),
     );
+  }
+}
+
+class TriviaControls extends StatefulWidget {
+  const TriviaControls({Key key}) : super(key: key);
+
+  @override
+  _TriviaControlsState createState() => _TriviaControlsState();
+}
+
+class _TriviaControlsState extends State<TriviaControls> {
+  final _controller = TextEditingController();
+  String _inputStr;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        TextField(
+          controller: _controller,
+          keyboardType: TextInputType.number,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            hintText: 'Input a number',
+          ),
+          onChanged: (value) {
+            _inputStr = value;
+          },
+        ),
+        SizedBox(height: 10),
+        Row(
+          children: <Widget>[
+            Expanded(
+              child: RaisedButton(
+                child: Text('Search'),
+                color: Theme.of(context).accentColor,
+                textTheme: ButtonTextTheme.primary,
+                onPressed: dispatchConcrete,
+              ),
+            ),
+            SizedBox(width: 10),
+            Expanded(
+              child: RaisedButton(
+                child: Text('Get random trivia'),
+                onPressed: () {},
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  void dispatchConcrete() {
+    _controller.clear();
+    BlocProvider.of<NumberTriviaBloc>(context)
+        .dispatch(GetTriviaForConcreteNumber(_inputStr));
   }
 }
